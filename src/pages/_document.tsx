878d79837615564d2d779912bdfbc847b6c56a81
taku1115 <59477643+taku1115@users.noreply.github.com>
2021-09-06 17:11:19 +0900

    🔍 add lang attribute

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import type { DocumentContext, DocumentInitialProps } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return initialProps;
+  }
+  // eslint-disable-next-line no-undef
+  render(): JSX.Element {
+    return (
+      <Html lang="ja">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
